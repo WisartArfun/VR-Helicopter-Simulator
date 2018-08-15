865b5003865de9eb700f23ce90f2f1cc1e6e4bc7
WisartArfun <39859700+WisartArfun@users.noreply.github.com>
2018-08-15 13:50:12 +0200

    Flying works

---
+++ VR Helicopter Simulator/Assets/Scripts/Helicopter/Base_Movement/Input_to_Movement.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Input_to_Movement : MonoBehaviour {
+
+	private Rigidbody rb;
+	private Transform helicopter;
+	public Transform rotor;
+	private Vector3 rotor_rotation;
+	public float force = 5;
+	
+	public float gravity_amount = 10.0f;
+
+	private Quaternion rotation_amount;
+	public float rotation_speed = 100;
+
+	private Vector3 new_forward_rotation;
+	public float forward_rotation_max_amount = 300;
+	private Quaternion forward_rotation;
+
+	private Vector3 new_sideward_rotation;
+	public float sideward_rotation_max_amount = 300;
+	private Quaternion sideward_rotation;
+	
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		helicopter = GetComponent<Transform>();
+		Physics.gravity = new Vector3(0, -gravity_amount, 0);
+		rotor_rotation = new Vector3(0, 0, 30);
+	}
+
+	public void Vertical_Movement(float amount) {
+		var end_amount = amount * Time.fixedDeltaTime * force;
+		if (Mathf.Abs(end_amount - gravity_amount) < 1f) {
+			end_amount = gravity_amount;
+		}
+		rb.AddForce(end_amount * helicopter.up);
+		// var new_rotation = rotor.rotation;
+		// new_rotation.y += 0.1f;
+		// rotor.rotation = new_rotation;
+		rotor.Rotate(rotor_rotation * 1);
+	}
+
+	public void Rotate(float amount) {
+		rotation_amount = Quaternion.Euler(0, rotation_speed * amount * Time.fixedDeltaTime, 0);
+		helicopter.rotation = rotation_amount * helicopter.rotation;
+	}
+
+	public void Forward(float amount) {
+		new_forward_rotation.Set(0, 0, forward_rotation_max_amount * amount * Time.fixedDeltaTime - helicopter.rotation.eulerAngles.z);
+		helicopter.Rotate(new_forward_rotation, Space.Self);
+		// forward_rotation = helicopter.rotation;
+		// forward_rotation.eulerAngles += ;
+		// fo
+
+	}
+
+	public void Sideward(float amount) {
+		new_sideward_rotation.Set(sideward_rotation_max_amount * amount * Time.fixedDeltaTime - helicopter.rotation.eulerAngles.x,  	0, 0);
+		helicopter.Rotate(new_sideward_rotation, Space.Self);
+	}
+}
---
+++ VR Helicopter Simulator/Assets/Scripts/Input/Local_Input.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Local_Input : MonoBehaviour {
+
+	private float h_Input;
+	private float yInput;
+	private float forward_Input;
+	private float sideward_Input;
+
+	public Controller_Keyboard controller_keyboard;
+	public GameObject receiver;
+	private Input_to_Movement receiver_input_controller;
+	public bool locked;
+	private float locked_value;
+	private float end_value;
+
+	void Start () {
+		controller_keyboard = GetComponent<Controller_Keyboard>();
+		receiver_input_controller = receiver.GetComponent<Input_to_Movement>();
+	}
+
+	void FixedUpdate () {
+		h_Input = Input.GetAxis("Vertical");  //somehow the axis is inverted
+		yInput = Input.GetAxis("Horizontal");
+
+		forward_Input = Input.GetAxis("Forward");
+		sideward_Input = Input.GetAxis("Sideward");
+
+		if (Input.GetKeyDown(KeyCode.Joystick1Button0)) {
+			if (locked) {
+				locked = false;
+			} else {
+				locked = true;
+			}
+			locked_value = h_Input;
+		}
+
+		// if (Mathf.Abs(h_Input) > 0.1) {
+		// if (h_Input > 0.1f) {
+		if (locked) {
+			h_Input += locked_value;
+		}
+		receiver_input_controller.Vertical_Movement(h_Input);
+		// }
+
+		Debug.Log(yInput);
+
+		if (Mathf.Abs(yInput) > 0.1f) {
+			receiver_input_controller.Rotate(yInput);
+		}
+
+		if (Mathf.Abs(forward_Input) > 0.1f) {
+			receiver_input_controller.Forward(forward_Input);
+		} else {
+			receiver_input_controller.Forward(0);
+		}
+
+		if (Mathf.Abs(sideward_Input) > 0.1f) {
+			receiver_input_controller.Sideward(sideward_Input);
+		} else {
+			receiver_input_controller.Sideward(0);
+		}
+
+
+		if (controller_keyboard.m_State == Controller_Keyboard.eInputState.MouseKeyboard) {
+		} else {
+		}
+	}
+}
