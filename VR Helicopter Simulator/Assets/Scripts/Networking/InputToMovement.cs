20283740e0609cd50bd35b4ccbae668750468c2e
WisartArfun <39859700+WisartArfun@users.noreply.github.com>
2018-09-08 16:21:10 +0200

    movement working but strong rubber band effect

--- VR Helicopter Simulator/Assets/Scripts/Networking/InputToMovement.cs
+++ VR Helicopter Simulator/Assets/Scripts/Networking/InputToMovement.cs
@@ -4,13 +4,83 @@ using UnityEngine;
 
 public class InputToMovement : MonoBehaviour {
 
+	private Rigidbody rb;
+	private Transform helicopter;
+	// public Transform rotor;
+	// private Vector3 rotor_rotation;
+	public float force = 5;
+	
+	public float gravity_amount = 10.0f;
+
+	private Quaternion rotation_amount;
+	public float rotation_speed = 100;
+
+	public float max_forward_rotation;
+	public float rotation_speed_forward;
+	private float target_rotation_forward;
+
+	public float max_sideward_rotation;
+	public float rotation_speed_sideward;
+	private float target_rotation_sideward;
+
+	public Transform start_pos;
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		helicopter = GetComponent<Transform>();
+		Physics.gravity = new Vector3(0, -gravity_amount, 0);
+		// rotor_rotation = new Vector3(0, 0, 30);
+		force =  gravity_amount / (0.5f * Time.fixedDeltaTime);
+	}
+
 	public void move_vertical(float force) {
 		GetComponent<Rigidbody>().AddForce(transform.up * force * 1);
-		Debug.Log("nice try");
 		transform.Translate(0.1f, 0, 0);
 	}
 
 	public void move_horizontal(float force) {
 		transform.Rotate(force * 1, 0, 0);
+	}
+
+	public void Vertical_Movement(float amount) {
+		var end_amount = 0f;;
+		if (Mathf.Abs(amount-0.5f) < 0.05f) {
+			end_amount = gravity_amount/ Time.fixedDeltaTime;
+		} else {
+			end_amount = amount * force;
+		}
+
+		if (end_amount > 0) {
+			rb.AddForce(end_amount * helicopter.up);
+		}
+		
+		// rotor.Rotate(rotor_rotation * 4.55f);
+	}
+
+	public void Rotate(float amount) {
+		rotation_amount = Quaternion.Euler(0, rotation_speed * amount, 0);
+		helicopter.rotation = rotation_amount * helicopter.rotation;
+	}
+
+	public void Forward(float amount) {
+		target_rotation_forward = max_forward_rotation * amount;
+		
+		var new_rot_f = Quaternion.Slerp(transform.localRotation, Quaternion.Euler(target_rotation_forward, transform.localRotation.eulerAngles.y, transform.localRotation.eulerAngles.z), rotation_speed_forward);
+
+		helicopter.rotation = new_rot_f;
+	}
+		
+
+	public void Sideward(float amount) {
+		target_rotation_sideward = max_sideward_rotation * amount;
+		
+		var new_rot_s = Quaternion.Slerp(transform.localRotation, Quaternion.Euler(transform.localRotation.eulerAngles.x, transform.localRotation.eulerAngles.y, target_rotation_sideward), rotation_speed_sideward);
+
+		helicopter.rotation = new_rot_s;
+	}
+
+	public void Reset() {
+		helicopter.position = start_pos.position;
+		helicopter.rotation = start_pos.rotation;
 	}
 }
--- VR Helicopter Simulator/Assets/Scripts/Networking/LocalHelicopterInput.cs
+++ VR Helicopter Simulator/Assets/Scripts/Networking/LocalHelicopterInput.cs
@@ -9,6 +9,27 @@ public class LocalHelicopterInput : NetworkBehaviour {
 	[SyncVar(hook="on_id_change")]
 	public NetworkInstanceId receiver_id;
 
+
+	private float v_Input;
+	private float yInput;
+	private float forward_Input;
+	private float sideward_Input;
+
+	public Controller_Keyboard controller_keyboard;
+	// public GameObject receiver;
+	private InputToMovement receiver_input_controller;
+	public bool locked;
+	private float locked_value;
+	private float end_value;
+
+	Checkpoints_Movement checkpoint_movement;
+
+	AllAroundInput input_mode;
+
+	public bool floating;
+
+	public OpenSettings open_settings;
+
 	void on_id_change(NetworkInstanceId id_new) {
 		receiver = ClientScene.FindLocalObject(id_new).GetComponent<InputToMovement>();
 
@@ -32,6 +53,30 @@ public class LocalHelicopterInput : NetworkBehaviour {
 		if (Input.GetKey(KeyCode.T)) {
 			receiver.move_horizontal(10f);
 		} 
+
+		horizontal_input(Input.GetAxis("Horizontal"), Time.deltaTime);
+
+		forward_input(-Input.GetAxis("Forward"));
+		sideward_input(-Input.GetAxis("Sideward"));
+		Debug.Log(Input.GetAxis("Forward"));
+
+		change_controller_checkpoint(Input.GetButton ("NextC"), Input.GetButton("PreviousC"));
+
+		if (Input.GetButton("Reset")) {
+			reset_helicopter();
+		}	
+
+		if (Input.GetButtonDown("Switch Floating")) {
+			switch_floating();
+		}
+
+		if (Input.GetButtonDown("Open Pause Menu")) {
+			open_settings.change_active();
+		}	
+
+		if (controller_keyboard.m_State == Controller_Keyboard.eInputState.MouseKeyboard) {
+		} else {
+		}
 	}
 
 	void FixedUpdate() {
@@ -42,5 +87,84 @@ public class LocalHelicopterInput : NetworkBehaviour {
 		if (Input.GetKey(KeyCode.U)) {
 			receiver.move_vertical(10f);
 		}
+
+		if (Input.GetKeyDown(KeyCode.Joystick1Button0)) {
+			lock_force_value();
+		}
+
+		vertical_movement(Input.GetAxis("Vertical"), Time.fixedDeltaTime);
+	}
+
+	void Start () {
+		controller_keyboard = GetComponent<Controller_Keyboard>();
+		receiver_input_controller = receiver.GetComponent<InputToMovement>();
+		checkpoint_movement = GetComponent<Checkpoints_Movement>();
+		input_mode = new AllAroundInput();
+		// receiver_input_controller.force = receiver_input_controller.gravity_amount * input_mode.gravity_times_force;
+	}
+
+	void horizontal_input(float input, float timestep) {
+		if (Mathf.Abs(input) > 0.1f) {
+			receiver_input_controller.Rotate(input_mode.adapt_input(input) * timestep);
+		}
+	}
+
+	void forward_input(float input) {
+		// Debug.Log("k1 " + input);
+		if (Mathf.Abs(input) > 0.1f) {
+			receiver_input_controller.Forward(input);
+		} else {
+			receiver_input_controller.Forward(0);
+		}
+	}
+
+	void sideward_input(float input) {
+		// Debug.Log("k3 " + input);
+		if (Mathf.Abs(input) > 0.1f) {
+			// receiver_input_controller.Sideward(input_mode.adapt_input(input));
+			receiver_input_controller.Sideward(input);
+		} else {
+			receiver_input_controller.Sideward(0);
+		}
+	}
+
+	void change_controller_checkpoint(bool previous, bool next) {
+		if (next) {
+			checkpoint_movement.change_checkpoint(1);
+		} else if (previous) {
+			checkpoint_movement.change_checkpoint(-1);
+		}
+	}
+
+	void reset_helicopter() {
+		receiver_input_controller.Reset();
+	}
+
+	void switch_floating() {
+		if (floating) {
+			floating = false;
+		} else {
+			floating = true;
+		}
+	}
+
+	void lock_force_value() {
+		if (locked) {
+			locked = false;
+		} else {
+			locked = true;
+		}
+		locked_value = v_Input;
+	}
+
+	void vertical_movement(float input, float timestep) {
+		if (locked) {
+			input += locked_value;
+		}
+		if (floating) {
+			input += input_mode.flying_controlled;
+		}
+		// Debug.Log(input_mode.adapt_input(input) * timestep);
+		receiver_input_controller.Vertical_Movement(input_mode.adapt_input(input) * timestep);
 	}
 }
